a62a8e3af8073b3013f4ea1d3975b2954da0067c
ChamodA <chamoda@wso2.com>
2024-07-08 14:36:12 +0530

    Test dm6

--- miproject/src/main/wso2mi/resources/registry/gov/datamapper/dm6/dm6.ts
+++ miproject/src/main/wso2mi/resources/registry/gov/datamapper/dm6/dm6.ts
@@ -3,21 +3,21 @@ interface Root {
         Type: number
         Id: string
     }[]
-    CreateDate: string
     ContentType: string
+    CreateDate: string
     Content: {
-        Message: string
-        Recipients: {
-            UserId: string
-            Name: string
-            EmailAddress: string
-        }[]
+        StatusDate: string
+        Position: {
+            Latitude: number
+            Longitude: number
+        }
+        Speed: number
+        Heading: number
+        IgnitionStatus: string
+        Odometer: number
+        Description: string
+        AdditionalDataElements: string
         TripInformation: {
-            TripName: string
-            Move: string
-            Leg: string
-            Stop: number
-            OrderHeader: number
             AdditionalDataElements: {
                 Label: string
                 Value: string
@@ -42,12 +42,20 @@ interface OutputRoot {
             ParentMessageGuid: string
             MessageContentType: string
             MessageContent: {
-                Message: string
-                Recipients: {
-                    UserId: string
-                    Name: string
-                    EmailAddress: string
-                }[]
+                StatusDate: string
+                Speed: number
+                Heading: number
+                Position: {
+                    Latitude: number
+                    Longitude: number
+                }
+                Description: string
+                IgnitionStatus: string
+                Odometer: number
+                Zip: string
+                City: string
+                State: string
+                Distance: string
                 TripInformation: {
                     TripName: string
                     Move: number
@@ -73,27 +81,28 @@ interface OutputRoot {
 }
 
 function mapFunction(input: Root): OutputRoot {
-    const MessageProperties_EventType = input.MessageProperties.EventType;
     return {
         dtos: [{
+            type: input.MessageProperties.EventType,
             data: {
                 MessageGuid: input.MessageProperties.MessageId,
                 ParentMessageGuid: input.MessageProperties.AuditId,
+                MessageContentType: input.MessageProperties.EventType,
                 MessageContent: {
+                    Distance: "",
+                    State: "",
+                    City: "",
+                    Zip: "",
+                    Odometer: floor(input.Content.Odometer),
+                    IgnitionStatus: input.Content.IgnitionStatus,
+                    Description: input.Content.Description,
+                    Heading: input.Content.Heading,
+                    Speed: input.Content.Speed,
+                    StatusDate: input.Content.StatusDate,
                     HeaderInformation: input.Content.HeaderInformation,
-                    Message: input.Content.Message,
-                    Recipients: input.Content.Recipients
-                        .map((RecipientsItem) => {
-                            return {
-                                UserId: RecipientsItem.UserId,
-                                Name: RecipientsItem.Name,
-                                EmailAddress: RecipientsItem.EmailAddress ?? ""
-                            }
-                        })
-                },
-                MessageContentType: input.MessageProperties.EventType
-            },
-            type: input.MessageProperties.EventType
+                    Position: input.Content.Position
+                }
+            }
         }]
     }
 }
@@ -101,4 +110,14 @@ function mapFunction(input: Root): OutputRoot {
 // WARNING: Do not edit/remove below function
 function map_S_root_S_root() {
     return mapFunction(inputroot);
+}
+
+/**
+ * #### AUTO-GENERATED FUNCTION, DO NOT MODIFY ####
+ * Finds the floor of a number.
+ * * @param num - The number to find the floor of.
+ * * @returns The floor of the number.
+ */
+function floor(num: number): number {
+    return Math.floor(num);
 }
